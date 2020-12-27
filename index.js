5acd71019d64e880eb0a89d52f9cddeafd6d9e5c
assassin_box <assassin543@outlook.jp>
2020-12-27 15:47:50 +0900

    DOM勉強

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const array=['0', '1', '2', '3'];
+const pTag=document.getElementById('test')
+const textBox=document.getElementById('text-box')
+const deleteBottn=document.getElementById('delete-botton')
 
-array.forEach[(nomber)=>{console.log(nomber)}]
+console.log(pTag);
 
-// for (let index = 0; index < array.length; index++) {
-//   const element = array[index];
-//   console.log(array[index]);
-// }
+deleteBottn.onclick =()=> {
+  console.log(textBox);
+}
+
+// const array=['0', '1', '2', '3'];
+
+// array.forEach[(nomber)=>{console.log(nomber)}]
