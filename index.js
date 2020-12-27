15854ebe5ab7795a010bce51674031d138d31719
assassin_box <assassin543@outlook.jp>
2020-12-27 14:53:29 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const array=['0', '1', '2', '3'];
+
+array.forEach[(nomber)=>{console.log(nomber)}]
+
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index];
+//   console.log(array[index]);
+// }
